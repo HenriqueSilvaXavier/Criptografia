943ac5329f40e2603118b6a5382173bb0e36e3d7
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2024-03-02 19:41:19 -0300

    Add files via upload

---
+++ Cifra de César 1.py
@@ -0,0 +1,19 @@
+alfa=["a", "b", "c", "d", "e" ,"f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+nome2=[]
+nome=input("Digite o nome: ")
+nome=nome.lower()
+for n in range(0, len(nome)):
+	nome2.append(nome[n])
+for n in range(0, len(nome2)):
+	if nome2[n]!=" ":
+		c=alfa.index(nome2[n])
+		if  c<24:
+			nome2[n]=alfa[c+1]
+		elif c==25:
+			nome2[n]="a"
+	else:
+		nome2[n]=" "
+nome=""
+for n in nome2:
+	nome="{}{}".format(nome, n)
+print(nome.title())
